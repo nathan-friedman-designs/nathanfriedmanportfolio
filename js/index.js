a4ae1ad6f0aa09bd53fac02ea62c77f72bea913e
Nathan Friedman <83198172+nathan-friedman-designs@users.noreply.github.com>
2022-10-03 08:34:18 -0500

    Update index.js
    adding js code for resume view overlay

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ function fade() {
     $('.preloader').fadeOut("slow");
     }
     setTimeout(fade, 3500);
+//for viewing resume
+function on() {
+    document.getElementById("overlayResume").style.display = "block";
+}
+function off() {
+    document.getElementById("overlayResume").style.display = "none";
+}
