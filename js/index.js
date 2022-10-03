609db63ee71c706834c2cbb506fcc5a1a23c7cbc
Nathan Friedman <83198172+nathan-friedman-designs@users.noreply.github.com>
2022-10-03 14:14:19 -0500

    Update index.js
    added code for right-oriented dropdown menu

--- js/index.js
+++ js/index.js
@@ -11,3 +11,31 @@ function on() {
 function off() {
     document.getElementById("overlayResume").style.display = "none";
 }
+//for viewing dropdown menu
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+//for closing dropdown menu
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropContent = document.getElementByClassName("dropdownContent");
+        var n;
+        for (n = 0; n < dropContent.length; n++) {
+            var openDropContent = dropContent[n];
+            if (openDropContent.classList.contains('show')) {
+                openDropContent.classList.remove('show');
+            }
+        }
+    }
+    if (!event.target.matches('.dropbtn')) {
+        var dropContent = document.getElementByClassName("dropdownContent2");
+        var n;
+        for (n = 0; n < dropContent.length; n++) {
+            var openDropContent = dropContent[n];
+            if (openDropContent.classList.contains('show')) {
+                openDropContent.classList.remove('show');
+            }
+        }
+    }
+}
