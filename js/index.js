493e286f17e254e877c57bc26d7118c2bea3d795
Nathan Friedman <83198172+nathan-friedman-designs@users.noreply.github.com>
2022-10-03 17:36:08 -0500

    Update index.js
    added code for js functions

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ function fade() {
     $('.preloader').fadeOut("slow");
     }
     setTimeout(fade, 3500);
+
 //for viewing resume
 function on() {
     document.getElementById("overlayResume").style.display = "block";
@@ -11,11 +12,13 @@ function on() {
 function off() {
     document.getElementById("overlayResume").style.display = "none";
 }
+
 //for viewing dropdown menu
 function dropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
     document.getElementById("myDropdown2").classList.toggle("show");
 }
+
 //for closing dropdown menu
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
@@ -39,3 +42,20 @@ window.onclick = function(event) {
         }
     }
 }
+
+//for copying text to clipboard
+function copyEmailText() {
+    //var copyText = document.getElementById("emailCopy");
+    navigator.clipboard.writeText("nathan.friedman.designs@gmail.com");
+    alert("Copied the text: nathan.friedman.designs@gmail.com");
+    }
+function copyLinkedInText() {
+        //var copyText = document.getElementById("linkedinCopy");
+        navigator.clipboard.writeText("http://www.linkedin.com/in/nathanfriedmandesigns");
+        alert("Copied the text: http://www.linkedin.com/in/nathanfriedmandesigns");
+        }
+function copyPortfolioText() {
+            //var copyText = document.getElementById("portfolioCopy");
+            navigator.clipboard.writeText("http://www.nathanfriedman.designs");
+            alert("Copied the text: http://www.nathanfriedman.designs");
+            }
